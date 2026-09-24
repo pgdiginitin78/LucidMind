bf6c2b57da6e763e14f7124f35486a7592ab7907
pgdiginitin78 <pgdiginitin78@gmail.com>
2026-09-24 16:44:14 +0530

    new ui changes

--- frontend/components/sections/contactUs/ContactUs.jsx
+++ frontend/components/sections/contactUs/ContactUs.jsx
@@ -1,6 +1,6 @@
 "use client";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { motion } from "framer-motion";
+import { motion,AnimatePresence} from "framer-motion";
 import { AlertCircle, CheckCircle2, Loader2, Send, X } from "lucide-react";
 import { Suspense, lazy, useEffect, useMemo, useState ,useRef} from "react";
 import { useForm } from "react-hook-form";
